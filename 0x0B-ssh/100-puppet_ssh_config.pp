81b77f1a5a73d058a93a6b4f214b6c112d500271
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-24 07:47:52 -0700

    ssh configuration using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,14 +4,14 @@ file { '/etc/ssh/ssh_config':
   ensure => present
 }
 
+file_line { 'identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentifyFile ~/.ssh/school',
+}
+
 file_line { 'password auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
-}
-
-file_line { 'identity files':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentifyFile ~/.ssh/school',
 }
