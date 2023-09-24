b5cd6e9831115cd6f1b0cc48162759721a6bd0b4
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-24 04:14:19 -0700

    using pippet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 # using puppet to connect without password
 
-file { '/root/.ssh/config':
+class ssh {
+file { '/etc/ssh/sshd_config':
   ensure  => present,
   owner   => 'root',
   group   => 'root',
   mode    => '0600',
   content => "IdentityFile /root/.ssh/school\nPasswordAuthentication no\n",
+}
 }
