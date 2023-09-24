5e69a25c76bb5131927a22501bf38fcb671f6628
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-23 20:07:07 -0700

    setting up identity using puppt

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 # using puppet to connect without password
 
-file { '/etc/ssh/ssh_config':
-  ensure => present,
-}
-
-file_line { 'turn off passwd auth':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'PasswordAuthentication no',
-  match => '^PasswordAuthentication',
-}
-
-file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentifyFile ~/.ssh/school',
-  match => '^IdentityFile',
+file { '/root/.ssh/config':
+  ensure  => present,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0600',
+  content => "IdentityFile /root/.ssh/school\nPasswordAuthentication no\n",
 }
