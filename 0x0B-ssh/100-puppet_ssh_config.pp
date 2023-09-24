9096c7430c321ec0a2f7c4b17815d7e912a13e80
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-24 10:18:40 -0700

    puppet configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,22 +1,17 @@
 # ensuring ssh config maintains state
 
-class { 'stdlib': }
-
 file {'/etc/ssh/ssh_config':
   ensure => present
 }
-include stdlib
 
-file_line {'Declare identity files':
+file_line {'identity files':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => '    IdentifyFile ~/.ssh/school',
-  replace => true,
 }
 
-file_line {'Turn off password auth':
+file_line {' password auth':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
-  replace => true,
 }
