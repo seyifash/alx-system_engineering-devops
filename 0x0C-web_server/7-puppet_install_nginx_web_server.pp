dc6b6fe15c63525b854bf6c3faee2bb6d6cf505e
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-26 02:14:00 -0700

    puppet to configure nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,13 +8,14 @@ file { 'var/www/html/index.html':
   content => 'Hello World!',
 }
 
-file_content { 'configs':
+file_line { 'configs':
   ensure => present,
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
+
 service { 'nginx':
   ensure  => running,
-  require => package['nginx'],
+  require => Package['nginx'],
 } 
