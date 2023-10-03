42021b67d9a09426d0981a1423b60de7d3abf3d8
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-03 02:12:11 -0700

    puppet config of custom header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,32 @@
+# configure to include a custom http header
+exec { 'update sytem':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure => 'installed',
+  require => Exec['update system']
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+file_line { 'configs':
+  ensure => present,
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file_line { 'HTTP header':
+  ensure => present,
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'server_name _;',
+  line   => 'add_header X-Served-By \$hostname;',
+}
+
+service { 'nginx':
+  ensure => running,
+  require => package['nginx'],
+}
