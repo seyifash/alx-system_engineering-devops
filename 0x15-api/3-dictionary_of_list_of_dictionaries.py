b9550b9fc82501b32e426615d46257dd66851eeb
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-23 14:01:59 -0700

    list all todo task of all employees using rest api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,25 +8,23 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            REST_AP = "https://jsonplaceholder.typicode.com"
-            emp_req = requests.get('{}/users'.format(REST_AP)).json()
-            filename = "todo_all_employees.json"
-            all_emp_task = {}
-            for emps in emp_req:
-                e_id = emps.get("id")
-                do_url = '{}/todos?userId={}'.format(REST_AP, e_id)
-                do_req = requests.get(do_url).json()
-                json_data = [
-                        {
-                            "username": emps["username"],
-                            "task": task["title"],
-                            "completed": task["completed"],
-                        }
-                        for task in do_req
-                    ]
+    REST_API = "https://jsonplaceholder.typicode.com"
+    emp_req = requests.get('{}/users'.format(REST_API)).json()
+    filename = "todo_all_employees.json"
+    all_emp_task = {}
+    for emps in emp_req:
+        e_id = emps["id"]
+        do_req = requests.get('{}/todos?userId={}'.format
+                              (REST_API, e_id)).json()
+        json_data = [
+            {
+                "username": emps["username"],
+                "task": task["title"],
+                "completed": task["completed"],
+            }
+            for task in do_req
+            ]
 
-                all_emp_task["e_id"] = json_data
-            with open(filename, 'w') as json_file:
-                json.dump(all_emp_task, json_file)
+        all_emp_task[e_id] = json_data
+    with open(filename, 'w') as json_file:
+        json.dump(all_emp_task, json_file)
