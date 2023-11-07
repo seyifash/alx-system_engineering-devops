ed1a822638f61f06adfcfcc66fab6ab51c712ec3
seyifash <fasugbeoluwaseyi@gmail.com>
2023-11-07 11:58:04 -0800

    getting count of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""checks for all subscribers"""
 import requests
 
 
@@ -6,24 +7,13 @@ def number_of_subscribers(subreddit):
     """ Define the Reddit API URL for the given subreddit """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    # Set a custom User-Agent to avoid being blocked
     headers = {
         'User-Agent': 'YourBotName/1.0 (by YourUsername)'
     }
-
-    # Send a GET request to the Reddit API without following redirects
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check the response status code
     if response.status_code == 200:
-        try:
-            # Parse the JSON response and extract the subscriber count
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        except (KeyError, ValueError):
-            # Handle JSON parsing errors
-            return 0
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        # Handle invalid subreddit or other errors
         return 0
