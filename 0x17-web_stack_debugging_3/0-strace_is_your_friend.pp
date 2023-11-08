1a3d5a74ffb463e6cd483d0469c8c5920c488e43
seyifash <fasugbeoluwaseyi@gmail.com>
2023-11-08 06:41:29 -0800

    fix wordpress

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix the file that has the line "phpp" instead of php in the file "wp-settings.php" which causes a 500 error
-exec {'fix-typographical-error':
+exec {'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
